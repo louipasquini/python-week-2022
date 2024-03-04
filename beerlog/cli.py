d23bab63d022b7528a2cc6389a5c1f1edcd40c5e
louipasquini <louipasquini@gmail.com>
2024-03-04 08:45:18 -0300

    Update: Format table

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,9 +1,12 @@
 import typer
 from typing import Optional
 from beerlog.core import add_beer_to_database, get_beers_from_database
+from rich.table import Table
+from rich.console import Console
 
 main = typer.Typer(help="Beer Management Application")
 
+console = Console()
 
 @main.command("add")
 def add(
@@ -30,4 +33,15 @@ def add(
 def list_beers(style: Optional[str] = None):
     """Lists beers in database"""
     beers = get_beers_from_database()
-    print(beers)
+    table = Table(title="Beerlog :beer_mug:")
+    headers = ["id","name","style","flavor","image","cost","rate","date"]
+
+    for header in headers:
+        table.add_column(header.capitalize(), style="magenta")
+    
+    for beer in beers:
+        beer.date = beer.date.strftime('%d/%m/%Y')
+        values = [str(getattr(beer, header)) for header in headers]
+        table.add_row(*values)
+    
+    console.print(table)
